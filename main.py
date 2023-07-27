fa61954472d49789e74e97567d2d813e8b487ec2
DillTheBeast <dillonmaltese@gmail.com>
2023-07-27 09:44:43 -0400

    Update main.py

--- main.py
+++ main.py
@@ -52,16 +52,16 @@ while running:
     pg.draw.rect(screen, BORDER_COLOR, (10, 228, bar_width, bar_height))
     pg.draw.rect(screen, BACKGROUND_COLOR, (12, 230, bar_width - 4, bar_height - 4))
     #Luffy
-    pg.draw.rect(screen, BORDER_COLOR, (WIDTH - 172, 198, bar_width, bar_height))
-    pg.draw.rect(screen, BACKGROUND_COLOR, (WIDTH - 170, 200, bar_width - 4, bar_height - 4))
+    pg.draw.rect(screen, BORDER_COLOR, (WIDTH - 172, 138, bar_width, bar_height))
+    pg.draw.rect(screen, BACKGROUND_COLOR, (WIDTH - 170, 140, bar_width - 4, bar_height - 4))
 
     # Draw the filled part of the health bar based on the health percentage
     #Naruto
     pg.draw.rect(screen, HEALTH_COLOR, (12, 230, bar_filled_width - 4, bar_height - 4))
     #Luffy
-    pg.draw.rect(screen, HEALTH_COLOR, (WIDTH - 170, 200, bar_filled_width - 4, bar_height - 4))
+    pg.draw.rect(screen, HEALTH_COLOR, (WIDTH - 170, 140, bar_filled_width - 4, bar_height - 4))
     screen.blit(scaledImageP, (0,250))
-    screen.blit(scaledImageE, (WIDTH - 220, 175))
+    screen.blit(scaledImageE, (WIDTH - 220, 125))
     screen.blit(scaledImageP1, (0, 100))
     screen.blit(scaledImageE1, (WIDTH - 200, 10))
 
